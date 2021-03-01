fab9eee50267a94fb15e488534e820e98b9c7a74
jigom11791 <jigom11791@mgail.com>
2021-03-01 14:03:11 -0800

    check if binary string is true

--- assignment6/minefinder.py
+++ assignment6/minefinder.py
@@ -8,9 +8,6 @@ def gen_functions(sweeper, grid, functions):
     for i in range(len(grid)):
         for j in range(len(grid)):
             if grid[i][j] != '0' and grid[i][j] != ' ':
-                if grid[i][j] not in functions:
-                    functions.update({grid[i][j]: []})
-
                 # Check neighbors in all directions to see if they are empty
                 neighbors = []
                 neighbors.clear()
@@ -26,9 +23,36 @@ def gen_functions(sweeper, grid, functions):
                     sweeper.flags.append(neighbors[0])
                 # if there are
                 elif len(neighbors) > 1:
+                    if grid[i][j] not in functions:
+                        functions.update({grid[i][j]: []})
                     functions[grid[i][j]].append(neighbors)
     return empty_neighbors
 
+
+def enumerate_combos(empty_neighbors):
+    # append to list if true
+    listOfStrings = []
+    diff = 0
+
+    for i in range(int('1'*len(empty_neighbors), 2)+1):
+        binstring = "0" + str(len(empty_neighbors)) + "b"
+        binary = format(i, binstring)
+        listOfStrings.append(binary)
+    print(listOfStrings)
+
+
+def check_if_true(bin_str, neighbors, flags, funct):
+    nm = {}
+    for i in range(len(bin_str)):
+        nm.update({neighbors[i]: bin_str[i]})
+    for key, value in funct.items():
+        for cord_list in value:
+            sum = 0
+            for cord in cord_list:
+                sum += int(nm[cord])
+            if sum != key:
+                return False
+    return True
 
 
 if __name__ == '__main__':
@@ -43,3 +67,4 @@ if __name__ == '__main__':
     print("functions:", functions)
     print("sweeper", sweeper.flags)
     print(int("111111", 2))
+    print(check_if_true("0"*len(empty_neighbors), empty_neighbors, sweeper.flags, functions))
