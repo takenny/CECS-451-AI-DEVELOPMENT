e366a2f351e0abae7137068817742bddf9ff3771
jigom11791 <jigom11791@mgail.com>
2021-03-01 19:15:28 -0800

    idk anymore

--- assignment6/minefinder.py
+++ assignment6/minefinder.py
@@ -10,6 +10,7 @@ def gen_functions(sweeper, functions):
     for i in range(len(grid)):
         for j in range(len(grid)):
             if grid[i][j] != '0' and grid[i][j] != ' ':
+
                 # Check neighbors in all directions to see if they are empty
                 neighbors = []
                 neighbors.clear()
@@ -32,6 +33,19 @@ def gen_functions(sweeper, functions):
     return empty_neighbors
 
 
+def get_neighbors(grid, i, j):
+    neighbors = []
+    neighbors.clear()
+    for x in range(-1, 2):
+        for y in range(-1, 2):
+            if 0 <= i + x < len(grid) and 0 <= j + y < len(grid):
+                if grid[i + x][j + y] == ' ':
+                    if (i + x, j + y) not in empty_neighbors:
+                        empty_neighbors.append((i + x, j + y))
+                    neighbors.append((i + x, j + y))
+
+
+
 def enumerate_combos(empty_neighbors, sweeper, functions):
     # append to list if true
     safe = []
@@ -41,9 +55,7 @@ def enumerate_combos(empty_neighbors, sweeper, functions):
     for i in range(int('1'*len(empty_neighbors), 2)+1):
         binstring = "0" + str(len(empty_neighbors)) + "b"
         binary = format(i, binstring)
-        # neighbors = format(i+1, binstring)
-        # if (i + 1 < len(empty_neighbors)) else neighbors = format(i+1, binstring)]
-        if(check_if_true(binary, empty_neighbors, sweeper.flags, functions)):
+        if check_if_true(binary, empty_neighbors, sweeper.flags, functions):
             listOfStrings.append(binary)
     transposed_list = list(map(list, zip(*listOfStrings)))
     for i in range(len(transposed_list)):
@@ -73,16 +85,21 @@ def check_if_true(bin_str, neighbors, flags, funct):
 
 if __name__ == '__main__':
     safe = []
-    gridsize = 10
-    n_mines = 10
+    gridsize = 16
+    n_mines = 30
     sweeper = Mines(gridsize, n_mines)
     sweeper.showcurrent()
     functions = {}
 
     while not sweeper.isfail() and not sweeper.checkmines():
         print(sweeper.isfail(), sweeper.checkmines())
+        print("starting gen functions")
         empty_neighbors = gen_functions(sweeper, functions)
+        print(empty_neighbors)
+        print("end gen functions")
+        print("start enumerate")
         safe = enumerate_combos(empty_neighbors, sweeper, functions)
+        print("end enumerate")
 
         for cell in safe:
             sweeper.checkcell(cell)
