16a0f12fb9971cac65105dbecf8ef82e133952b3
jigom11791 <jigom11791@mgail.com>
2021-03-01 16:28:45 -0800

    fixed check if true

--- assignment6/minefinder.py
+++ assignment6/minefinder.py
@@ -21,15 +21,14 @@ def gen_functions(sweeper, grid, functions):
                 # if there is only one neighbor it can be added to the flag list
                 if len(neighbors) == 1 and neighbors[0] not in sweeper.flags:
                     sweeper.flags.append(neighbors[0])
-                # if there are
-                elif len(neighbors) > 1:
+                if len(neighbors) >= 1:
                     if grid[i][j] not in functions:
                         functions.update({grid[i][j]: []})
                     functions[grid[i][j]].append(neighbors)
     return empty_neighbors
 
 
-def enumerate_combos(empty_neighbors):
+def enumerate_combos(empty_neighbors, flags, functions):
     # append to list if true
     listOfStrings = []
     diff = 0
@@ -37,25 +36,28 @@ def enumerate_combos(empty_neighbors):
     for i in range(int('1'*len(empty_neighbors), 2)+1):
         binstring = "0" + str(len(empty_neighbors)) + "b"
         binary = format(i, binstring)
-        #neighbors = format(i+1, binstring)
-        #if (i + 1 < len(empty_neighbors)) else neighbors = format(i+1, binstring)]
-        if(check_if_true(binary, empty_neighbors, sweeper.flags, functions)):
+        # neighbors = format(i+1, binstring)
+        # if (i + 1 < len(empty_neighbors)) else neighbors = format(i+1, binstring)]
+        if(check_if_true(binary, empty_neighbors, flags, functions)):
             listOfStrings.append(binary)
 
-        #listOfStrings.append(binary)
-    print(listOfStrings)
+        # listOfStrings.append(binary)
+    print("list:", listOfStrings)
 
 
 def check_if_true(bin_str, neighbors, flags, funct):
     nm = {}
+    print(functions)
     for i in range(len(bin_str)):
         nm.update({neighbors[i]: bin_str[i]})
     for key, value in funct.items():
         for cord_list in value:
             sum = 0
             for cord in cord_list:
+                if cord in flags and nm[cord] == 0:
+                    return False
                 sum += int(nm[cord])
-            if sum != key:
+            if sum != int(key):
                 return False
     return True
 
@@ -72,5 +74,6 @@ if __name__ == '__main__':
     #print("functions:", functions)
     #print("sweeper", sweeper.flags)
     #print(int("111111", 2))
-    print(check_if_true("0"*len(empty_neighbors), empty_neighbors, sweeper.flags, functions))
-    print(enumerate_combos(empty_neighbors))
+    # print(check_if_true("0"*len(empty_neighbors), empty_neighbors, sweeper.flags, functions))
+    enumerate_combos(empty_neighbors, sweeper.flags, functions)
+    print(sweeper.isfail())
