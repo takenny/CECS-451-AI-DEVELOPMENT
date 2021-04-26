614b1ed1554b8cea0d05b66650133115dc293d9d
Kenny Ta <55076129+takenny@users.noreply.github.com>
2021-04-26 13:10:33 -0700

    yupdated
    need help getting ffoler name

--- Assignment 11 attached files Apr 23, 2021 626 PM/speech.py
+++ Assignment 11 attached files Apr 23, 2021 626 PM/speech.py
@@ -1,4 +1,7 @@
+import pathlib
+
 import distance
+import speech_recognition as sr
 
 class Speech:
 	def __init__(self):
@@ -14,7 +17,20 @@ class Speech:
 
 
 	def conv_audio(self, inDir):
-		print("uh")
+		print('in')
+		r = sr.Recognizer()
+		harvard = sr.AudioFile('08-Sent01.wav')
+		with harvard as source:
+			audio = r.record(source)
+		print(r.recognize_google(audio))
+		#for file in inDir:
+		#	print('yep')
+		#	audio = sr.AudioFile(file)
+		#	with audio as source:
+		#		print("uh")
+		#		test = r.record(source)
+		#		r.recognize_google(test)
+
 
 
 	def comp_string(self):
@@ -26,4 +42,4 @@ class Speech:
 if __name__ == '__main__':
 	s = Speech()
 	s.read_original("How Speech Recognition Works.txt")
-	print(s.original)
+	audio = s.conv_audio("inDir")
